9116a1f89c11d6ccf08e10446567e28a802a751e
Yash Patel <ympatel132635@gmail.com>
2022-11-19 13:18:40 +0530

    Practical-10

---
+++ app/src/main/java/com/example/madpractical10_20012011136/MapsActivity.kt
@@ -0,0 +1,46 @@
+package com.example.madpractical10_20012011136
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.madpractical10_20012011136.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private val TAG = "MapActivity"
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+    private var lat = -34.0
+    private var log = 151.0
+    private var title = "Marker in Sydney"
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val obj = intent.getSerializableExtra("Object") as Person
+        Log.i(TAG, "onCreate: Object:$obj")
+        lat = obj.Latitude
+        log = obj.Longitude
+        title = obj.Name
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val sydney = LatLng(lat, log)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
